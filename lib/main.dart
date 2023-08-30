6746f36e5ed18723f59e9577b7eeec22127a28e4
uniK007 <kislayk970@gmail.com>
2023-08-30 19:59:59 +0530

    Main feature-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// main test feature -4
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
